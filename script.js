1a7a2b2195ab40eace536e0e33c35e9b44ef2bfa
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-11-27 17:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const text = document.getElementById("text");
+const delay = document.getElementById("delay");
+const button = document.getElementById("btn");
+
+// const p = new Promise((res, rej) =>{
+// 	setTimeout(() =>{
+//         console.log(text.value);
+// 		res(text.value);
+// 	},delay.value);
+// });
+
+async function display(){
+	// const data = await p;
+    // console.log(data);
+    setTimeout(() =>{
+        document.getElementById("output").innerText = text.value;
+    },delay.value);
+}
+
+button.addEventListener("click", display);
